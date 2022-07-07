9915bd652aedc3adbb8b8a09ca51aacad99059e7
Yaya KONE <koneyayangolo@gmail.com>
2022-07-07 20:05:43 +0200

    second iteration yaya

---
+++ coffee_logic/src/DrinkMaker.java
@@ -0,0 +1,6 @@
+public class DrinkMaker {
+
+    public static void processCommand(String c){
+        System.out.println("Making command: "+ c);
+    }
+}
--- coffee_logic/src/Main.java
+++ coffee_logic/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String command = "Chocolate,2";
         IDrink drink = Drink.parse(command);
+        DrinkMaker.processCommand(drink.getCommand());
         System.out.println(drink.getCommand());
     }
 }
