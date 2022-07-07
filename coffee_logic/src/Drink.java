594cdc436d6110164829f254663659254e5c4b8a
Yaya KONE <koneyayangolo@gmail.com>
2022-07-07 18:58:27 +0200

    first iteration

---
+++ coffee_logic/src/Drink.java
@@ -0,0 +1,14 @@
+public class Drink implements  IDrink{
+    private String type;
+    private int sugar;
+
+    @Override
+    public String getCommand(){
+        return "";
+    }
+
+    Drink(String type, int sugar){
+
+    }
+
+}
