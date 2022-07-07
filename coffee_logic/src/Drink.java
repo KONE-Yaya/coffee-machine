558b8dc5f6beae7c53aa6599d9ed05cfac7e07b9
Sofiane BRANECI <sofiane.braneci@sita.aero>
2022-07-07 19:22:41 +0200

    Drink impl

--- coffee_logic/src/Drink.java
+++ coffee_logic/src/Drink.java
@@ -4,11 +4,49 @@ public class Drink implements  IDrink{
 
     @Override
     public String getCommand(){
-        return "";
+        StringBuilder builder = new StringBuilder();
+        builder.append(type).append(":");
+        if(sugar > 0) builder.append(sugar).append(":").append("0");
+        else builder.append(":");
+
+        return builder.toString();
     }
 
-    Drink(String type, int sugar){
-
+    public Drink(String type, int sugar){
+        this.type = type;
+        this.sugar = sugar > 2 ? 2 : sugar;
     }
+    Drink(String type){
+        this(type, 0);
+    }
+
+    // drink,sugarCount
+    public static IDrink parse(String drink){
+        String [] parts = drink.split(",");
+        String [] types = {"Tea", "Coffee", "Chocolate"};
+        String type = "";
+        for(String t : types){
+            if(t.equals(parts[0])) {
+                type = t;
+                break;
+            }
+        }
+
+        switch(type) {
+            case "Tea":
+                type = "T";
+                break;
+            case "Chocolate":
+                type = "H";
+                break;
+            default:
+                type = "C";
+        }
+
+        int sugar = Integer.valueOf(parts[1]);
+        if(sugar > 0) return new Drink(type, sugar);
+        return new Drink(type);
+    }
+
 
 }
--- coffee_logic/src/Main.java
+++ coffee_logic/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+
+    public static void main(String[] args) {
+        String command = "Chocolate,2";
+        IDrink drink = Drink.parse(command);
+        System.out.println(drink.getCommand());
+    }
 }
